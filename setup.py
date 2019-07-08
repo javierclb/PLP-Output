b51b62eb319592ea333095ab11a68568be7fa5df
javierclb <javierclb>
2019-07-08 12:36:53 -0400

    reinitialized files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PLP_Output',
+    version='0.0.1',
+    description='Libreria para tratamiento de salidas PLP',
+    packages=['PLP_Output'],
+    author='Javier De la Fuente Arce',
+    author_email='jdelafuente@colbun.cl',
+    keywords=['PLP'],
+)
