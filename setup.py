4a586848a8eab3090b47ba29042e2c98f1ad2c5f
javierclb <javierclb>
2019-07-18 09:33:12 -0400

    update

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup
 
 setup(
     name='PLP_Output',
-    version='0.0.1',
+    version='0.0.2',
     description='Libreria para tratamiento de salidas PLP',
     packages=['PLP_Output'],
     author='Javier De la Fuente Arce',
     author_email='jdelafuente@colbun.cl',
+    url='git@github.com:javierclb/PLP_Output.git',
+    download_url='https://github.com/javierclb/PLP_Output/tree/master/dist',
     keywords=['PLP'],
+    licence='MIT',
+    install_requires=['pandas']
 )
